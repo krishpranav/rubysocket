cba7b872b40cd6636734a2523ff07ee6d1f5097d
krishpranav <krisna.pranav@gmail.com>
2021-12-28 18:56:02 +0530

    lib: web socket[error + self class]

---
+++ lib/web_socket.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require "base64"
+require "socket"
+require "uri"
+require "digest/md5"
+require "digest/sha1"
+require "openssl"
+require "stringio"
+
+class WebSocket
+
+    class << self
+        attr_accessor(:debug)
+    end
+    
+    class Error < RuntimeError
+    end
+
+    WEB_SOCKET_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+    OPCODE_CONTINUATION = 0x00
+    OPCODE_TEXT = 0x01
+    OPCODE_BINARY = 0x02
+    OPCODE_CLOSE = 0x08
+    OPCODE_PING = 0x09
+    OPCODE_PONG = 0x0a
+
+
+end
