ab7a7a9f0ba7bb2ac9ef7838ded5586301626060
krishpranav <krisna.pranav@gmail.com>
2021-12-28 18:54:14 +0530

    lib: parser
    parsing

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,3 +1,11 @@
 module Parser
     @regexp = /([^:]+):([0-9]+)?(\+)?:([^:]+)?:?([\s\S]*)?/
+  
+    def self.decode(string)
+      if pieces = string.match(@regexp)
+        {type: pieces[1], id: pieces[2], end_point: pieces[4], data: pieces[5]}
+      else
+        {type: '0'}
+      end
+    end
 end
